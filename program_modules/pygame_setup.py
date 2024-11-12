94325c2257c59fe3d202b77d3ad8e7b9500a0b7d
Egor <139051554+EgorGalkinORG@users.noreply.github.com>
2024-11-12 22:51:11 +0200

    Added raw version of button

--- program_modules/pygame_setup.py
+++ program_modules/pygame_setup.py
@@ -1,17 +1,21 @@
 # create pygame screen 800x600
 import pygame
 from .widgets.pygame_image import PygameImage
+from .widgets.pygame_button import PygameButton
 
 def start_game():
     pygame.init()
     game = True
     screen = pygame.display.set_mode(size = (800, 600))
     background_image = PygameImage(screen, "sea_battle_SPRINT/static/images/great_sea_battle_bg.png", (800, 600))
+    start_button = PygameButton(screen, "sea_battle_SPRINT/static/images/start_button.png", (150, 50))
     pygame.display.update()
 
-
     while game:
+        #print("work")
+        
         for event in pygame.event.get():
+            start_button.click_checking(event)
             if event.type == pygame.QUIT:
                 game = False
         
---
+++ program_modules/widgets/pygame_button.py
@@ -0,0 +1,12 @@
+import pygame
+from .pygame_image import PygameImage
+import sys
+
+class PygameButton():
+    def __init__(self, screen, path, size):
+        button_image = PygameImage(screen, path, size)        
+    def click_checking(self, event):
+        #mouse = pygame.mouse.get_pos()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print(2232)
+                
