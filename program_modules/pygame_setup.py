449bd7ab9e13971e1cf52f0b68e650009a6048c5
vadila234git <155548075+vadila234git@users.noreply.github.com>
2024-11-13 23:26:14 +0200

    make class for button

--- program_modules/pygame_setup.py
+++ program_modules/pygame_setup.py
@@ -10,23 +10,13 @@ class Game():
         game = True
         screen = pygame.display.set_mode(size = (800, 600))
 
-        
-        
-
-        # start_button = PygameButton(screen, "static/images/start_button.png", (150, 50))
-        
-
         while game:
-            pygame_event = pygame.event.get()
+            event = pygame.event.get()
             background_image = PygameImage(screen, "static/images/great_sea_battle_bg.png", (0, 0), (800, 600))
-            start_button = PygameButton(screen, "static/images/start_button.png", (100, 100), (100, 40), pygame_event)
-            
+            start_button = PygameButton(screen, "static/images/start_button.png", (100, 100), (100, 40), event)
 
-            for event in pygame.event.get():
-                
-
-                # start_button.click_checking(event)
-                if event.type == pygame.QUIT:
+            for pygame_event in event:
+                if pygame_event.type == pygame.QUIT:
                     game = False
 
             pygame.display.update()
