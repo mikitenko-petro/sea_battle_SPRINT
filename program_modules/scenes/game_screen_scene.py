d4cb1a426e477a0612cbe83bfe857dd79cb70013
David Petrenko <121516101+Davidptn@users.noreply.github.com>
2024-11-18 22:59:10 +0200

    made cells

--- program_modules/scenes/game_screen_scene.py
+++ program_modules/scenes/game_screen_scene.py
@@ -1,10 +1,12 @@
 import pygame
 from ..widgets.pygame_image import PygameImage
+from ..game_modules.grid import Grid
 
 class GameScreneScene():
     def __init__(self, screen : object, scene_manager : object):
         self.screen = screen
         self.scene_manager = scene_manager
+        self.grid = Grid()
 
     def run(self, event : object):
         background_image = PygameImage(
@@ -12,3 +14,19 @@ class GameScreneScene():
         path = "static/images/sea_bg.png",
         coordinates = (0, 0),
         size = (1200, 700))
+
+        cell_x = 0
+        cell_y = 0
+
+        for row in self.grid.grid:
+            for column in row:
+                if column == '':
+                    cell = PygameImage(
+                        screen = self.screen,
+                        path = "static/images/cell.png",
+                        coordinates = (cell_x, cell_y),
+                        size = (50, 50))
+                    
+                cell_x += 60 
+            cell_y += 60
+            cell_x = 0
