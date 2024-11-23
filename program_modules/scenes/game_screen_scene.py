c2a12e7fc5ff7cb3ad6646a4433b65275424009e
vadila234git <155548075+vadila234git@users.noreply.github.com>
2024-11-23 21:34:47 +0200

    some changes

--- program_modules/scenes/game_screen_scene.py
+++ program_modules/scenes/game_screen_scene.py
@@ -8,52 +8,53 @@ class GameScreneScene():
         self.scene_manager = scene_manager
         self.player_grid = Grid()
         self.enemy_grid = Grid()
+
     def run(self, event : object):
         background_image = PygameImage(
         screen = self.screen,
         path = "static/images/sea_bg.png",
         coordinates = (0, 0),
         size = (1200, 700))
-        
-        enemy_cell_x = 0
-        enemy_cell_y = 0
+
+        for ship_x in range(4): 
+            ship = PygameImage(
+            coordinates = (150 + ship_x*50, 50),
+            size = (50, 50),
+            screen = self.screen,
+            path ="static/images/ship1X1.png")
 
         player_cell_x = 0
         player_cell_y = 0
-
 
         for row in self.player_grid.grid:
             for column in row:
                 if column == '':
                     cell = PygameImage(
-                        screen = self.screen,
-                        path = "static/images/cell.png",
-                        coordinates = (50 + player_cell_x, 150 + player_cell_y),
-                        size = (50, 50))
+                    screen = self.screen,
+                    path = "static/images/cell.png",
+                    coordinates = (50 + player_cell_x, 150 + player_cell_y),
+                    size = (50, 50))
                     
                 player_cell_x += 50
             player_cell_y += 50
             player_cell_x = 0
 
+        enemy_cell_x = 0
+        enemy_cell_y = 0
+
         for row in self.enemy_grid.grid:
             for column in row:
                 if column == '':
                     cell = PygameImage(
-                        screen = self.screen,
-                        path = "static/images/cell.png",
-                        coordinates = (650 + enemy_cell_x, 150 + enemy_cell_y),
-                        size = (50, 50))
+                    screen = self.screen,
+                    path = "static/images/cell.png",
+                    coordinates = (650 + enemy_cell_x, 150 + enemy_cell_y),
+                    size = (50, 50))
                     
                 enemy_cell_x += 50
             enemy_cell_y += 50
             enemy_cell_x = 0
 
-        for ship_x in range(4): 
-            ship = PygameImage(
-                coordinates = (150 + ship_x*50, 50),
-                size = (50, 50),
-                screen = self.screen,
-                path ="static/images/ship1X1.png"
-            )
+        
 
             
