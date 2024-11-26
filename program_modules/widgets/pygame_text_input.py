bd110bda6c7c58907d679f3375f9a24c42419560
David Petrenko <121516101+Davidptn@users.noreply.github.com>
2024-11-26 23:11:27 +0200

    make text field

--- program_modules/widgets/pygame_text_input.py
+++ program_modules/widgets/pygame_text_input.py
@@ -10,9 +10,12 @@ class PygameTextInput():
         self.x, self.y = coordinates
         self.width, self.height = size
         self.pygame_storage.add_variable({"status" : "not selected"})
+        self.pygame_storage.add_variable({"user_text" : ""})
+        self.pygame_storage.add_variable({"active" : False})
 
         self.click_checking(event)
         self.show()
+        self.input_text(event)
 
     def click_checking(self, event):
         mouse_x, mouse_y = pygame.mouse.get_pos()
@@ -34,25 +37,32 @@ class PygameTextInput():
             size = (self.width, self.height),
             path = "static/images/input_field.png")
 
-            self.input_text = PygameText(
-            screen = self.screen,
-            text = "Enter ip adress",
-            font = None,
-            font_size = 50,
-            x = self.x+10,
-            y = self.y+30)
+            self.pygame_storage.storage_dict["user_text"] = "Enter ip adress"
+
         else:
             self.input_image = PygameImage(
             screen = self.screen,
             coordinates = (self.x, self.y),
             size = (self.width, self.height),
             path = "static/images/input_field_selected.png")
-
-            self.input_text = PygameText(
+            self.pygame_storage.storage_dict["user_text"] = ""
+        
+        self.input_text_lable = PygameText(
             screen = self.screen,
-            text = "",
+            text = self.pygame_storage.storage_dict["user_text"],
             font = None,
             font_size = 50,
             x = self.x+10,
             y = self.y+30)
+
+    def input_text(self, event):
+        for pygame_event in event:
+            if pygame_event.type == pygame.QUIT:
+                pygame.quit()   
+            if pygame_event.type == pygame.KEYDOWN:
+                if pygame_event.key == pygame.K_BACKSPACE:
+                    self.pygame_storage.storage_dict["user_text"] = self.pygame_storage.storage_dict["user_text"[:-1]]
+                else:
+                    self.pygame_storage.storage_dict["user_text"] += pygame_event.unicode
+                    print(self.pygame_storage.storage_dict["user_text"])
         
